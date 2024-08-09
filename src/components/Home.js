649242f762aef20f18120967fb500813a9e18ca6
Abdulazeez Saheed Olawale <saheedazeez2003@gmail.com>
2024-08-09 17:34:35 +0100

    Update home page text size and social links btn size

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,11 @@ const Home = () => {
     return (
         <>
             <div
-                className="grid md:grid-cols-2 items-center justify-center gap-40  mx-5 px-3 min-h-screen"
+                className="grid lg:grid-cols-2 items-center justify-center gap-40  mx-5 px-3 min-h-screen"
                 id="home">
-                <div className="px-5 pt-20 text-center md:text-left">
+                <div className="px-10 pt-20 text-center md:text-left">
                     <p className="text-blue-500 text-xl hidden md:block">Hi,</p>
-                    <h1 className="text-white md:text-7xl text-4xl font-bold font-serif">
+                    <h1 className="text-white md:text-6xl text-4xl font-bold font-serif">
                         I'M ABDULAZEEZ, SAHEED OLAWALE
                     </h1>
                     <h2 className="text-2xl text-blue-400">
@@ -19,9 +19,12 @@ const Home = () => {
                         skill set to include frontend development.
                     </p>
                     <div className="mt-4">
-                    <a href="/saheedResume.pdf" download={true} className="bg-gray-300 hover:bg-white transition font-bold py-3 px-4 rounded text-blue-800">
-                        Get Resume
-                    </a>
+                        <a
+                            href="/Saheed-Olawale-Abdulazeez-Resume.pdf"
+                            download={true}
+                            className="bg-gray-300 hover:bg-white transition font-bold py-3 px-4 rounded text-blue-800">
+                            Get Resume
+                        </a>
                     </div>
                 </div>
                 <div>
@@ -29,7 +32,7 @@ const Home = () => {
                         src="/assets/program.svg"
                         alt="."
                         width="70%"
-                        className="hidden md:block rounded-lg"
+                        className="hidden lg:block rounded-lg"
                     />
                 </div>
             </div>
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -64,7 +64,7 @@ const SocialLinks = () => {
                 {links.map(({ id, child, href, style, download }) => (
                     <li
                         key={id}
-                        className={`flex justify-between items-center w-40 h-14 px-4 ml-[-100px] md:hover:ml-[-10px] hover:rounded-md duration-300  ${style}`}>
+                        className={`flex justify-between items-center w-36 h-12 px-3 ml-[-100px] md:hover:ml-[-10px] hover:rounded-md duration-300  ${style}`}>
                         <a
                             href={href}
                             className="flex justify-between items-center w-full text-white"
